e57a9f21ffecde304d55cb7011b6989b1d78b66a
nimesh607 <48921126+nimesh607@users.noreply.github.com>
2025-02-24 10:06:16 +0530

    Add files via upload

---
+++ Polymorphism/Polymorphism/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    internal class Class1
+    {
+        int a = 10;
+
+        public void Display ()
+        {
+            Console.WriteLine("Hello I am Display method  From Class 1 ");
+        }
+
+        public static void Show()
+        {
+            Console.WriteLine("Hello I am show method  From Class 1 ");
+        }
+
+        public virtual void Show2()
+        {
+            Console.WriteLine("Hello I am show2 method  From Class 1 ");
+        }
+
+    }
+}

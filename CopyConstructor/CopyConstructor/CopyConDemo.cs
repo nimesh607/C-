4002173e28ca3a96f7600faae6bc88780de140d7
nimesh607 <48921126+nimesh607@users.noreply.github.com>
2025-05-09 14:24:47 +0530

    Update CopyConDemo.cs

--- CopyConstructor/CopyConstructor/CopyConDemo.cs
+++ CopyConstructor/CopyConstructor/CopyConDemo.cs
@@ -14,14 +14,14 @@ namespace CopyConstructor
      
         public CopyConDemo( int id)
         {
-            this.id = id;
+            this.id = id;//loading values from Db just for example
             name = "Vijay";
             balance = 5000.00;
         }
 
-        public CopyConDemo(CopyConDemo cd )
+        public CopyConDemo(CopyConDemo cd )// string can be a parameter than CopyConDemo can also be a parameter because it is class just like string
         {
-            this.id = cd.id;
+            this.id = cd.id;//Now for the new instance we are Copying value from old instance to new instance 
             name = cd.name;
             balance = cd.balance;
             

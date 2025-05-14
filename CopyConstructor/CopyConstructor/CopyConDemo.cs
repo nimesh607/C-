4542ebbadb40199dfac24384afde95c43be2c5b4
nimesh607 <48921126+nimesh607@users.noreply.github.com>
2025-05-14 18:29:17 +0530

    Add files via upload

---
+++ CopyConstructor/CopyConstructor/CopyConDemo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyConstructor
+{
+    internal class CopyConDemo
+    {
+        public int id;
+         public string name;
+        double balance;
+     
+        public CopyConDemo( int id)
+        {
+            this.id = id;
+            name = "Vijay";
+            balance = 5000.00;
+        }
+
+        public CopyConDemo(CopyConDemo cd )
+        {
+            this.id = cd.id;
+            name = cd.name;
+            balance = cd.balance;
+            
+        }
+
+
+        public void Display()
+        {
+            Console.WriteLine($"Id : {id}; Name{name}; Balance{balance}");
+        }
+    }
+}
+                                                                           
---
+++ CopyConstructor/CopyConstructor/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace CopyConstructor
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            CopyConDemo obj1 = new CopyConDemo(105);
+            Console.WriteLine(obj1);
+            obj1.Display();
+            CopyConDemo obj2 = new CopyConDemo(obj1);
+            obj2.Display();
+            Console.WriteLine();
+            obj1.balance = 1000.00;
+
+
+        }
+    }
+}

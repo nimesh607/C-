048d6c757e874b40d1ac4350037d81ed3fbfd6f6
nimesh607 <48921126+nimesh607@users.noreply.github.com>
2025-05-14 18:26:21 +0530

    Add files via upload

---
+++ AbstractClass/AbstractClass/AbsParent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    internal abstract class AbsParent
+    {
+        public   void add(int a, int b)
+        {
+            Console.WriteLine(a += b);
+        }
+
+        public void sub(int a, int b) //Abstract class will also have non abstract  method (concreate method)
+        {
+            Console.WriteLine(a -= b);
+        }
+
+        public abstract  void mul(int a, int b); //1) abstract members should only be in abstract class .it will not allow in non abstract class to have abstract members
+        public abstract void div(int a, int b); //2) There will be no instance of abstract class
+        //3)Abstract class is parent class
+
+        //3)As there is no instace of abstract class to access the member of this ,it's only possoble  through child class.
+
+    }
+}

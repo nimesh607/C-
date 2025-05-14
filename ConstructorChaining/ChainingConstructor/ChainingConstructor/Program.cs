db25eaec7da32f7cec4357ed10300efd35523674
nimesh607 <48921126+nimesh607@users.noreply.github.com>
2025-05-14 18:28:34 +0530

    Add files via upload

---
+++ ConstructorChaining/ChainingConstructor/ChainingConstructor/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace ChainingConstructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Dog d1 = new Dog();
+           // Dog d2 = new Dog("Rocky");
+           // Dog d3 = new Dog("Max", 5);
+
+        }
+    }
+}

7d1c23c68c960e254924b404e290ec2cb537b481
nimesh607 <48921126+nimesh607@users.noreply.github.com>
2025-05-14 18:30:28 +0530

    Add files via upload

---
+++ Inheritance/Inheritance/Class3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    public class Class3
+    {
+         int x = 5;
+        public void Display()
+        {
+            Console.WriteLine(x);
+        }
+       public class Class4
+        {
+            public static void Main()
+            {
+                Class3 obj = new Class3();
+                obj.Display();
+            }
+        }
+    }
+}

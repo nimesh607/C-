6a05525b5caccf88edd172a6b4795d062b7d5d29
nimesh607 <48921126+nimesh607@users.noreply.github.com>
2025-05-14 18:32:20 +0530

    Add files via upload

---
+++ Polymorphism/Polymorphism/Class2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    internal class Class2:Class1
+    {
+        public override void Show2()
+        {
+            Console.WriteLine("Hello I am show2 method  From Class 2 ");
+        }
+
+        public new void Show() //Hiding/Shadowing using new keyword. 2)Static methods can't be override becaause it's belong to class.3)Here it will treat as  new instance member
+        {
+            Console.WriteLine("Hello I am show method  From Class 2 ");
+        }
+        
+        public void Show3()
+        {
+            base.Show2();//you can call by using base keyword parent version of method even thoug if you overrriden it.
+        }
+    }
+}
---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class2 obj = new Class2();
+            obj.Show();
+            obj.Show2();
+            Class1 class1 = obj;
+            obj.Show();
+            obj.Show3();
+        
+        }
+    }
+}

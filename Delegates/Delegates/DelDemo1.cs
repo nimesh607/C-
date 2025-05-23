4641342b98a65c5b1ca6fb430fecfcd3d91d018c
nimesh607 <nuvaishnav607@gmail.com>
2025-05-23 18:46:15 +0530

    Delegates

---
+++ Delegates/Delegates/DelDemo1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    //Note: while defining a delegate you should follow the same signature of the method i.e., parameters of delegateshould be same as the parameters of method and return types of delegates should be same as the return types of method, we want to call by using the delegate.
+
+    public delegate void CalculatorDelegate(int a,int b,int c);//Step1:Definition of a delegate 
+    public delegate string WishDelegate(string Name);
+    internal class DelDemo1
+    {
+        public void AddNums(int x, int y, int z)
+        {
+            Console.WriteLine("Sum of given 3 no's is:" + (x + y + z));
+        }
+
+        public static string SayHello(string Name)
+        {
+            return $"Hello {Name},how are you";
+        }
+
+        static void Main(string[] args)
+        {
+            //Step2:Instantiate the delegate.In this process we create the instance of the delegate and bind the method we want to   call by using the delegate to the delegate.
+
+            WishDelegate wishDelegate =new WishDelegate(DelDemo1.SayHello);//Binding  method with delegate .Another way=>   WishDelegate wishDelegate=DelDemo1.SayHello
+            Console.WriteLine(wishDelegate("Alex"));
+            Console.WriteLine(wishDelegate("Monash"));
+
+
+
+            DelDemo1 demo1 = new DelDemo1();//nonstatic so first make instance
+            CalculatorDelegate calculatorDelegate = new CalculatorDelegate(demo1.AddNums);//Binding  method with delegate.Another way to instiate demo1.AddNums
+            calculatorDelegate(10,20,30); calculatorDelegate(100, 205, 310);
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Delegates/Delegates/DelDemo2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    public delegate void MathDelegate(int a, int b);
+    internal class DelDemo2
+    {
+        public void Add(int x, int y)
+        {
+            Console.WriteLine($"Add: {x + y}");
+        }
+        public void Sub(int x, int y)
+        {
+            Console.WriteLine($"Sub: {x - y}");
+        }
+        public void Mul(int x, int y)
+        {
+            Console.WriteLine($"Mul: {x * y}");
+        }
+
+        public void Div(int x, int y)
+        {
+            Console.WriteLine($"Div: {x / y}");
+        }
+
+        static void Main()
+        {
+            //create instance of class first because methods are non static
+            DelDemo2 delDemo2 = new DelDemo2();
+            MathDelegate md = delDemo2.Add;
+            md += delDemo2.Sub;//MultiCast Delegates.binding multiple methods with single delegate.Muliticasting can be done with only non-returning method i.e.,void 
+            md += delDemo2.Mul;//Multicast Delegates.include
+            md(100, 300);
+            Console.WriteLine();
+            md(250, 50);//one call four results.no need to make 4 calls.1 calls four methods will execute
+            Console.WriteLine();
+            //Now i do not want multiply than do like this
+            md -= delDemo2.Mul;//exclude
+            md(400, 20);
+        }
+    }
+}

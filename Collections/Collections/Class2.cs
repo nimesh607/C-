a00310b0cff020dda209ad0d86c9f88d70c71c40
nimesh607 <nuvaishnav607@gmail.com>
2025-05-27 00:12:02 +0530

    Collections

---
+++ Collections/Collections/Class2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    internal class Class2
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+            list.Add(1);
+            list.Add(true);
+            list.Add(67.90);
+            list.Add("Hello");
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i] + " ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Collections/Collections/Program.cs
@@ -0,0 +1,40 @@
+﻿ global using System.Collections;
+namespace Collections
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Stack stack = new Stack();
+            stack.Push(1); stack.Push(false);
+            stack.Push(34.56);
+            stack.Push(DateTime.Now);
+            stack.Push("Nine");
+           
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item + " ");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Pop Operation : " + stack.Pop());
+
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item + " ");
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Peek Operation : " + stack.Peek());// returns topmost element present in stack
+
+            Console.WriteLine(stack.Count);
+            Console.WriteLine(stack.Clear);
+
+        }
+    }
+}

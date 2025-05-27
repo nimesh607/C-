d7915a38e2f2f8b814b6dedb733cf4167e138c2d
nimesh607 <nuvaishnav607@gmail.com>
2025-05-27 16:33:23 +0530

    hashtables

--- Collections/Collections/Class2.cs
+++ Collections/Collections/Class2.cs
@@ -11,16 +11,51 @@ namespace Collections
         static void Main(string[] args)
         {
             ArrayList list = new ArrayList();
+            Console.WriteLine("Initial Capacity : " + list.Capacity);
             list.Add(1);
+            Console.WriteLine("After adding 1 element Capacity is  : " + list.Capacity);//auto-risizing doubles capacity when there is no space
             list.Add(true);
             list.Add(67.90);
             list.Add("Hello");
+            list.Add(true);
 
             for (int i = 0; i < list.Count; i++)
             {
                 Console.WriteLine(list[i] + " ");
                 Console.WriteLine();
             }
+
+            // list.Remove((true));//first occurence
+            //list.RemoveAt(4);//3rd index, 2nd occurence
+             list.RemoveRange(2, 2);
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write(list[i] + " ");
+                Console.WriteLine();
+            }
+
+            ArrayList list2 = new ArrayList(50);//allocation capacity through parameterized capacity
+            ArrayList list3 = new ArrayList(list);//used copy constructor 
+            Console.WriteLine("Initial Capacity of list3 : " + list3.Capacity);
+            for (int i = 0;i < list3.Count; i++)
+            {
+                Console.Write(list3[i] + " ");
+                Console.WriteLine( );
+            }
+
+            list3.Add("Hello");
+            list3.Add("Animals");
+
+            Console.WriteLine("capacity of after adding elements in existing array : " + list3.Capacity);
+            for (int i = 0; i < list3.Count; i++)
+            {
+                Console.Write(list3[i] + " ");
+                Console.WriteLine();
+            }
+             list3.TrimToSize();
+
+            Console.WriteLine("after trimming  : " + list3.Capacity);
         }
     }
 }
---
+++ Collections/Collections/Class3.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    internal class Class3
+    {
+        static void Main(string[] args)
+        {
+            Hashtable Emp = new Hashtable();//user defined keys
+            Emp.Add("Emp-Id", 1001);
+            Emp.Add("Emp-Name", "Scott");
+            Emp.Add("Job", "CEO");
+            Emp.Add("Mgr-Id", null);
+            Emp.Add("Salary", 50000.00);
+            Emp.Add("Commission", 0.00f);
+            Emp.Add("Dept-Id", 10);
+            Emp.Add("Dept-Name", "Administration");
+            Emp.Add("Location", "Mumbai");
+            Emp.Add("Status", true);
+            Emp.Add("PAN", "AKYPM 1234K");
+            Emp.Add("Adhar No.", "1234 5678 9012");
+            Emp.Add("Mobile", "98392 14256");
+            Emp.Add("Home Phone", "2718 6547");
+            Emp.Add("Email", "Scott@gmail.com");
+
+            Console.WriteLine(Emp["Emp-Name"]);
+
+            foreach(object key in Emp.Keys)
+            {
+                Console.WriteLine(key + ":" + Emp[key]);
+            }
+        }
+    }
+}

0cfff95189966dec3be09a6859918986757c8441
nimesh607 <nuvaishnav607@gmail.com>
2025-09-04 23:07:09 +0530

    listbox,Checkboc,listcheckbox

---
+++ DemoCbLbClb/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace DemoCbLbClb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "India",
+            "United States",
+            "Canada",
+            "Australia",
+            "China",
+            "New Zealand",
+            "France"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 22);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 33);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.comboBox1_KeyPress);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(139, 22);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiSimple;
+            this.listBox1.Size = new System.Drawing.Size(120, 79);
+            this.listBox1.TabIndex = 1;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(265, 22);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(207, 82);
+            this.checkedListBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
+
---
+++ DemoCbLbClb/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoCbLbClb
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //To add items in listbox :<ListControl>.Items.Add("item");
+            listBox1.Items.Add("Rajasthan");
+            listBox1.Items.Add("Gujarat");
+            listBox1.Items.Add("MadhyaPradesh");
+            listBox1.Items.Add("AndhraPradesh");
+            listBox1.Items.Add("Assam");
+
+            string[] cities = { "Jaipur", "Udaipur", "Kota", "Bikaner", "Jodhpur" ,"Hyderabad"};
+            //To add multiple items in listbox :<ListControl>.Items.AddRange(array);
+            checkedListBox1.Items.AddRange(cities);
+
+            //To add items from datasource:
+            //Step1:<ListControl>.DataSource = <database Table>;
+            //Step2:<ListControl>.DisplayMember = "<column name to be displayed>";
+
+        }
+
+        private void comboBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Add item dynamically to combobox when user press Enter key 
+            if (Convert.ToInt32(e.KeyChar)==13) //13 is ASCII value of Enter key
+            {
+                if (comboBox1.FindStringExact(comboBox1.Text) == -1)
+                    {
+                    comboBox1.Items.Add(comboBox1.Text);
+                   // comboBox1.Text = "";
+                }
+            }
+        }
+    }
+}

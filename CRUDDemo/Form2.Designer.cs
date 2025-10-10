bd886e0d2b9ff9161c02838136b8fe3c61212390
nimesh607 <nuvaishnav607@gmail.com>
2025-10-10 13:33:49 +0530

    Added Data Adapter and DataSet

---
+++ CRUDDemo/Form2.Designer.cs
@@ -0,0 +1,296 @@
+﻿namespace CRUDDemo
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox4 = new TextBox();
+            label5 = new Label();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            label4 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            btnFirst = new Button();
+            btnPrevious = new Button();
+            btnLast = new Button();
+            btnNext = new Button();
+            btnNew = new Button();
+            btnInsert = new Button();
+            btnUpdate = new Button();
+            btnDelete = new Button();
+            btnSaveDb = new Button();
+            btnSearch = new Button();
+            btnClose = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.FromArgb(255, 255, 192);
+            label1.Dock = DockStyle.Top;
+            label1.Font = new Font("Segoe UI Semibold", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(0, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(377, 49);
+            label1.TabIndex = 1;
+            label1.Text = "Employee Details";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(102, 223);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(241, 23);
+            textBox4.TabIndex = 16;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(11, 223);
+            label5.Name = "label5";
+            label5.Size = new Size(41, 15);
+            label5.TabIndex = 15;
+            label5.Text = "Salary:";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(102, 168);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(241, 23);
+            textBox3.TabIndex = 14;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(102, 112);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(241, 23);
+            textBox2.TabIndex = 13;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(102, 57);
+            textBox1.Name = "textBox1";
+            textBox1.ReadOnly = true;
+            textBox1.Size = new Size(241, 23);
+            textBox1.TabIndex = 12;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(11, 168);
+            label4.Name = "label4";
+            label4.Size = new Size(28, 15);
+            label4.TabIndex = 11;
+            label4.Text = "Job:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(11, 112);
+            label3.Name = "label3";
+            label3.Size = new Size(46, 15);
+            label3.TabIndex = 10;
+            label3.Text = "Ename:";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(11, 57);
+            label2.Name = "label2";
+            label2.Size = new Size(30, 15);
+            label2.TabIndex = 9;
+            label2.Text = "Eno:";
+            // 
+            // btnFirst
+            // 
+            btnFirst.Location = new Point(12, 252);
+            btnFirst.Name = "btnFirst";
+            btnFirst.Size = new Size(86, 41);
+            btnFirst.TabIndex = 17;
+            btnFirst.Text = "First";
+            btnFirst.UseVisualStyleBackColor = true;
+            btnFirst.Click += btnFirst_Click;
+            // 
+            // btnPrevious
+            // 
+            btnPrevious.Location = new Point(104, 252);
+            btnPrevious.Name = "btnPrevious";
+            btnPrevious.Size = new Size(86, 41);
+            btnPrevious.TabIndex = 18;
+            btnPrevious.Text = "Prev";
+            btnPrevious.UseVisualStyleBackColor = true;
+            btnPrevious.Click += btnPrevious_Click;
+            // 
+            // btnLast
+            // 
+            btnLast.Location = new Point(288, 252);
+            btnLast.Name = "btnLast";
+            btnLast.Size = new Size(78, 41);
+            btnLast.TabIndex = 19;
+            btnLast.Text = "Last";
+            btnLast.UseVisualStyleBackColor = true;
+            btnLast.Click += btnLast_Click;
+            // 
+            // btnNext
+            // 
+            btnNext.Location = new Point(196, 252);
+            btnNext.Name = "btnNext";
+            btnNext.Size = new Size(86, 41);
+            btnNext.TabIndex = 20;
+            btnNext.Text = "Next";
+            btnNext.UseVisualStyleBackColor = true;
+            btnNext.Click += btnNext_Click;
+            // 
+            // btnNew
+            // 
+            btnNew.Location = new Point(12, 299);
+            btnNew.Name = "btnNew";
+            btnNew.Size = new Size(86, 41);
+            btnNew.TabIndex = 21;
+            btnNew.Text = "New";
+            btnNew.UseVisualStyleBackColor = true;
+            btnNew.Click += btnNew_Click;
+            // 
+            // btnInsert
+            // 
+            btnInsert.Location = new Point(102, 299);
+            btnInsert.Name = "btnInsert";
+            btnInsert.Size = new Size(86, 41);
+            btnInsert.TabIndex = 22;
+            btnInsert.Text = "Insert";
+            btnInsert.UseVisualStyleBackColor = true;
+            btnInsert.Click += btnInsert_Click;
+            // 
+            // btnUpdate
+            // 
+            btnUpdate.Location = new Point(196, 299);
+            btnUpdate.Name = "btnUpdate";
+            btnUpdate.Size = new Size(86, 41);
+            btnUpdate.TabIndex = 23;
+            btnUpdate.Text = "Update";
+            btnUpdate.UseVisualStyleBackColor = true;
+            btnUpdate.Click += btnUpdate_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Location = new Point(288, 299);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(78, 41);
+            btnDelete.TabIndex = 24;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = true;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // btnSaveDb
+            // 
+            btnSaveDb.Location = new Point(12, 346);
+            btnSaveDb.Name = "btnSaveDb";
+            btnSaveDb.Size = new Size(176, 33);
+            btnSaveDb.TabIndex = 25;
+            btnSaveDb.Text = "Save to Database";
+            btnSaveDb.UseVisualStyleBackColor = true;
+            btnSaveDb.Click += btnSaveDb_Click;
+            // 
+            // btnSearch
+            // 
+            btnSearch.Location = new Point(194, 342);
+            btnSearch.Name = "btnSearch";
+            btnSearch.Size = new Size(86, 41);
+            btnSearch.TabIndex = 26;
+            btnSearch.Text = "Search";
+            btnSearch.UseVisualStyleBackColor = true;
+            btnSearch.Click += btnSearch_Click;
+            // 
+            // btnClose
+            // 
+            btnClose.Location = new Point(286, 342);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new Size(80, 40);
+            btnClose.TabIndex = 27;
+            btnClose.Text = "Close";
+            btnClose.UseVisualStyleBackColor = true;
+            btnClose.Click += btnClose_Click;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(377, 390);
+            Controls.Add(btnClose);
+            Controls.Add(btnSearch);
+            Controls.Add(btnSaveDb);
+            Controls.Add(btnDelete);
+            Controls.Add(btnUpdate);
+            Controls.Add(btnInsert);
+            Controls.Add(btnNew);
+            Controls.Add(btnNext);
+            Controls.Add(btnLast);
+            Controls.Add(btnPrevious);
+            Controls.Add(btnFirst);
+            Controls.Add(textBox4);
+            Controls.Add(label5);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form2";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Form2";
+            Load += Form2_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox4;
+        private Label label5;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Button btnFirst;
+        private Button btnPrevious;
+        private Button btnLast;
+        private Button btnNext;
+        private Button btnNew;
+        private Button btnInsert;
+        private Button btnUpdate;
+        private Button btnDelete;
+        private Button btnSaveDb;
+        private Button btnSearch;
+        private Button btnClose;
+    }
+}
---
+++ CRUDDemo/Form2.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Data.SqlClient;
+using Microsoft.VisualBasic;
+
+namespace CRUDDemo
+{
+    public partial class Form2 : Form
+    {
+        int RowIndex = 0;//Index of current row of DataTable
+
+        DataSet ds;
+        SqlConnection con;
+        SqlCommand cmd;
+        SqlDataAdapter da;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            btnInsert.Enabled = false;
+            con = new SqlConnection("Data Source=localhost;Database=CSDB;Integrated Security=SSPI;TrustServerCertificate=true");
+
+            /*
+             * there are 4 constructor  of data adapter class 
+             */
+            cmd = new SqlCommand("Select Eno,Ename,Job,Salary From Employee Where Status=1 Order by Eno", con);
+            da = new SqlDataAdapter(cmd);  //with command object
+
+            da.MissingSchemaAction = MissingSchemaAction.AddWithKey;//it will add primary key information to DataTable while filling the DataSet object
+
+            ds = new DataSet();
+            da.Fill(ds, "Employee");//Data Set is loaded with data 
+
+            showData();
+
+            /*
+             * this is another way to fill the DataSet object
+             cmd=new SqlCommand("Select Eno,Ename,Job,Salary From Employee Order by Eno",con);
+            da=new SqlDataAdapter(); //default constructor
+            da.selectcommand=cmd;  
+            ds = new DataSet();  
+             da.Fill(ds,"Employee");
+             */
+
+            /*
+            * this is another way to fill the DataSet object
+           da=new SqlDataAdapter("Select Eno,Ename,Job,Salary From Employee Order by Eno",con); //no need to create command object.da will create command object internally
+           da.selectcommand=cmd;  
+           ds = new DataSet();  
+            da.Fill(ds,"Employee");
+            */
+
+
+            /*
+            * this is another way to fill the DataSet object
+           da=new SqlDataAdapter("Select Eno,Ename,Job,Salary From Employee Order by Eno","Data Source=localhost;Database=CSDB;Integrated Security=SSPI;TrustServerCertificate=true"); //no need to create command object.da will create command object internally and no need to create connection object also.da will create connection object internally
+           ds = new DataSet();  
+            da.Fill(ds,"Employee");
+            */
+
+        }
+
+        private void showData()
+        {
+            if (ds.Tables["Employee"].Rows[RowIndex].RowState != DataRowState.Deleted)//to check whether the current row is deleted or not ,by checking the RowState property of DataRow class.
+            {
+                textBox1.Text = ds.Tables["Employee"].Rows[RowIndex]["Eno"].ToString();
+                textBox2.Text = ds.Tables["Employee"].Rows[RowIndex]["Ename"].ToString();
+                textBox3.Text = ds.Tables["Employee"].Rows[RowIndex]["Job"].ToString();
+                textBox4.Text = ds.Tables["Employee"].Rows[RowIndex]["Salary"].ToString();
+            }
+            else
+            {
+                MessageBox.Show("Current Row is deleted and can't be accessed");
+            }
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            if (RowIndex < ds.Tables[0].Rows.Count - 1)
+            {
+                RowIndex += 1;
+                showData();
+            }
+            else
+            {
+                MessageBox.Show("You are at last record", "Information", MessageBoxButtons.OK);
+            }
+
+        }
+
+        private void btnPrevious_Click(object sender, EventArgs e)
+        {
+            if (RowIndex > 0)
+            {
+                RowIndex -= 1;
+                showData();
+            }
+            else
+            {
+                MessageBox.Show("You are at first record", "Information", MessageBoxButtons.OK);
+            }
+        }
+
+        private void btnFirst_Click(object sender, EventArgs e)
+        {
+            RowIndex = 0;
+            showData();
+        }
+
+        private void btnLast_Click(object sender, EventArgs e)
+        {
+            RowIndex = ds.Tables[0].Rows.Count - 1;
+            showData();
+        }
+
+
+
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = textBox2.Text = textBox3.Text = textBox4.Text = "";//this way we can assign multiple textboxes to empty string.without repeating the assignment operator
+
+            int lastRowIndex = ds.Tables[0].Rows.Count - 1;
+            int newEno = Convert.ToInt32(ds.Tables[0].Rows[lastRowIndex]["Eno"]) + 1;//Incrementing the last Eno by 1 to get new Eno.returns object type data so we are using Convert.ToInt32 method to convert it into int type data
+
+            textBox1.Text = newEno.ToString();
+            btnInsert.Enabled = true;
+
+            textBox2.Focus();//it will set the focus to textBox2 control
+        }
+
+        private void btnSaveDb_Click(object sender, EventArgs e)
+        {
+            /* 
+             * Manually defining the InsertCommand,UpdateCommand and DeleteCommand of DataAdapter object
+             * da.InsertCommand.CommandText = "Insert Stmt";
+             da.UpdateCommand.CommandText = "Update Stmt";
+             da.DeleteCommand.CommandText = "Delete Stmt";
+            */
+
+            SqlCommandBuilder builder = new SqlCommandBuilder(da);//it will automatically generate the InsertCommand,UpdateCommand and DeleteCommand of DataAdapter object based on the SelectCommand of DataAdapter object.
+
+            da.Update(ds, "Employee");//it will save the changes made in DataSet to database table
+
+            MessageBox.Show("Changes Saved Successfully to Database", "Success", MessageBoxButtons.OK);
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            DataRow dr = ds.Tables["Employee"].NewRow();//Creating new row in DataTable.Row is a instance of DataRow class
+            dr[0] = textBox1.Text;
+            dr[1] = textBox2.Text;
+            dr[2] = textBox3.Text;
+            dr[3] = textBox4.Text;
+            ds.Tables["Employee"].Rows.Add(dr);//Adding new row to DataTable.Rows is a instance of DataRowCollection class
+            btnInsert.Enabled = false;
+            RowIndex = ds.Tables[0].Rows.Count - 1;//Setting the RowIndex to last row index because new row is added at the end of DataTable.
+            MessageBox.Show("Record Inserted Successfully into DataSet", "Success", MessageBoxButtons.OK);
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            ds.Tables["Employee"].Rows[RowIndex]["Ename"] = textBox2.Text;
+            ds.Tables["Employee"].Rows[RowIndex]["Job"] = textBox3.Text;
+            ds.Tables["Employee"].Rows[RowIndex]["Salary"] = textBox4.Text;
+            MessageBox.Show("Record Updated Successfully in DataSet", "Success", MessageBoxButtons.OK);
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            ds.Tables["Employee"].Rows[RowIndex].Delete();
+            btnFirst.PerformClick();//it will perform click event of btnFirst button.this is done to show the first record after deleting the current record.
+
+            MessageBox.Show("Record Deleted Successfully from DataSet", "Success", MessageBoxButtons.OK);
+
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            //Import Microsoft.VisualBasic assembly to use InputBox function
+            string value=Interaction.InputBox("Enter an employee number to search", "Search Employee");
+            bool status = int.TryParse(value,out int Eno);
+            if (status)
+            {
+                DataRow dr = ds.Tables[0].Rows.Find(Eno);//Find method of DataRowCollection class is used to search a row in DataTable based on primary key value.it returns DataRow type data.
+
+                if(dr != null)
+                {
+                    RowIndex = ds.Tables[0].Rows.IndexOf(dr);//IndexOf method of DataRowCollection class is used to get the index of a row in DataTable based on DataRow object.it returns int type data.
+                    textBox1.Text = dr["Eno"].ToString();
+                    textBox2.Text = dr["Ename"].ToString();
+                    textBox3.Text = dr["Job"].ToString();
+                    textBox4.Text = dr["Salary"].ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Employee Number not found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Employee No must be an integer value.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
